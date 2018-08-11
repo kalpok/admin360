4947f5b44397a2e5a48b9b8de159dd1af3913fbb
Amirreza <amirreza.developer@gmail.com>
2018-08-11 19:35:47 +0430

    write javascript and css for sliding form and add them to ThemeAssetB… (#2)
    * write javascript and css for sliding form and add them to ThemeAssetBundle.php
    
    * remove unnecessary scrolling in sliding form because jquery  slideUp function has built in functionality for scrolling
    
    * add tool buttons to panel header

--- assetbundles/ThemeAssetBundle.php
+++ assetbundles/ThemeAssetBundle.php
@@ -8,9 +8,11 @@ class ThemeAssetBundle extends AssetBundle
     public $sourcePath = '@themes/admin360/assets';
     public $css = [
         'css/custom.css',
+        'css/sliding-form.css'
     ];
     public $js = [
         'js/app.js',
+        'js/sliding-form.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
